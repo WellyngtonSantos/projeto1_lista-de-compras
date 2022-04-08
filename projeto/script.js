913a8e9c7e8607bb3f0ad3faa30cdf7a38686d47
WellyngtonSantos <98568556+WellyngtonSantos@users.noreply.github.com>
2022-04-08 20:21:42 -0300

    atualiza comentários nos documentos

--- projeto/script.js
+++ projeto/script.js
@@ -1,17 +1,20 @@
+//cria espaços da memória para receber os dados enviados pelo HTML
 const texto = document.querySelector('input')
 const ul = document.querySelector('ul')
 const btnDeleteAll = document.querySelector('.header button')
 const btnInsert = document.querySelector('.divInsert button')
 
+//cria variável que recebe array de itens
 var itensDB = []
 
+//evento do botão de inserir o item (botão enter)
 texto.addEventListener('keypress', e => {
   if (e.key == 'Enter' && texto.value != '') {
     setItemDB()
   }
 })
 
-//evento do botão de inserir o item
+//evento do botão de inserir o item (clique do mouse)
 btnInsert.onclick = () => {
   if (texto.value != '') {
     setItemDB()
@@ -24,34 +27,31 @@ function setItemDB() {
     alert('Limite máximo de 20 itens atingido!')
     return
   }
-
   itensDB.push({ 'item': texto.value, 'status': '' })
   updateDB()
 }
 
-//lança o item dentro do localStorage como todolist e passa o itensDB pra JSON.stringfy,
-//aí depois que colocar no localStorage, chama uma função pra carregar os itens
 function updateDB() {
+  //lança o item dentro do localStorage como todolist e passa o itensDB pra JSON.stringfy,
   localStorage.setItem('todolist', JSON.stringify(itensDB))
+  //depois que adiciona no localStorage, chama uma função pra carregar os itens
   loadItens()
 }
 
-//limpa a ul pra não duplicar registros
-//busca os itens com um forEach
 //cria uma função pra inserir os itens na tela
 function loadItens() {
+  //limpa a ul pra não duplicar registros
   ul.innerHTML = "";
   itensDB = JSON.parse(localStorage.getItem('todolist')) ?? []
+  //busca os itens com um forEach
   itensDB.forEach((item, i) => {
     insertItemTela(item.item, item.status, i)
   })
 }
 
 //cria a li com o document.createElement pra poder criar a checkbox e atribuir o status do item
-//cria um if pra a classe que vai riscar o item ou não
 function insertItemTela(text, status, i) {
-  const li = document.createElement('li')
-  
+  const li = document.createElement('li')  
   li.innerHTML = `
     <div class="divLi">
       <input type="checkbox" ${status} data-i=${i} onchange="done(this, ${i});" />
@@ -60,18 +60,17 @@ function insertItemTela(text, status, i) {
     </div>
     `
   ul.appendChild(li)
-
+  //cria um if pra a classe que vai riscar o item ou não
   if (status) {
     document.querySelector(`[data-si="${i}"]`).classList.add('line-through')
   } else {
     document.querySelector(`[data-si="${i}"]`).classList.remove('line-through')
   }
-
+  //limpa o input inserindo uma string vazia após salvar o item
   texto.value = ''
 }
 
 //função done da checkbox pra quando marcar como realizado ele atribuir o status de checado
-//chama a função updateDB pra atualizar
 function done(chk, i) {
 
   if (chk.checked) {
@@ -79,17 +78,16 @@ function done(chk, i) {
   } else {
     itensDB[i].status = '' 
   }
-
+  //chama a função updateDB pra atualizar
   updateDB()
 }
 
 //cria a função para remover 1 item
-//chama o loadItens pra atualizar a tela
 function removeItem(i) {
   itensDB.splice(i, 1)
   updateDB()
 }
-
+//chama o loadItens pra atualizar a tela
 loadItens();
 
 //cria o delete all pra deletar todos os itens
